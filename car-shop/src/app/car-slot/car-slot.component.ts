1f09a2613dae3d7493a4849f8b4511640ad1bcca
Omar Abdelrazzaq <omar.1997aq@gmail.com>
2022-07-23 23:20:25 +0300

    my version of the project

--- car-shop/src/app/car-slot/car-slot.component.ts
+++ car-shop/src/app/car-slot/car-slot.component.ts
@@ -1,24 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { Post } from '../post.module';
 
 @Component({
   selector: 'app-car-slot',
   templateUrl: './car-slot.component.html',
-  styleUrls: ['./car-slot.component.css']
+  styleUrls: ['./car-slot.component.css'],
 })
 export class CarSlotComponent implements OnInit {
-
-  constructor(
-    private http: HttpClient) {
-      this.getData()
-     }
-     carData:any=this.getData()
-     getData():any{
-      return this.http.get('https://run.mocky.io/v3/793607a0-cdbb-4af4-91be-41f694b94ba9').subscribe(res => console.log(res))
-        }
-  ngOnInit(): void {
+  allProducts: Post[] = [];
+  index = 0;
+  constructor(private http: HttpClient) {
+    this.getData();
   }
-  
+  carData: any = this.getData();
+  getData(): any {
+    return this.http
+      .get<{ Post }>(
+        'https://run.mocky.io/v3/06caae7f-c6c8-4468-b09e-ed5673839d34'
+      )
+      .pipe(
+        map((res) => {
+          const products: Post[] = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              products.push({ ...res[key] });
+            }
+          }
+          return products;
+        })
+      )
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
+        console.log(this.allProducts);
+      });
+  }
 
+  onIncrease() {
+    if (this.index == this.allProducts.length - 1) {
+      this.index = 0;
+    } else this.index += 1;
+    console.log(this.index);
+  }
+
+  onDecrease() {
+    if (this.index <= 0) {
+      this.index = this.allProducts.length - 1;
+    } else this.index -= 1;
+    console.log(this.index);
+  }
+  ngOnInit(): void {}
 }
---
+++ car-shop/src/app/post.module.ts
@@ -0,0 +1,7 @@
+export interface Post {
+  id: number;
+  year: string;
+  price: string;
+  carType: string;
+  carImage: string;
+}

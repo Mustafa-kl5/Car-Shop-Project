7eb3590bdf21251646a5311767fc8f808b053207
Mustafa Mahmood <mmalowisi19@cit.just.edu.jo>
2022-07-24 01:23:00 +0300

    make requset

--- car-shop/src/app/car-slot/car-slot.component.ts
+++ car-shop/src/app/car-slot/car-slot.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Cars } from '../shared/cars';
+import { RestApiService } from '../shared/rest-api.service';
 
 @Component({
   selector: 'app-car-slot',
@@ -8,16 +8,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./car-slot.component.css']
 })
 export class CarSlotComponent implements OnInit {
+  cars : any
+  constructor(private carService:RestApiService) {
+    
+  }
+    
 
-  constructor(
-    private http: HttpClient) {
-      this.getData()
-     }
-     carData:any=this.getData()
-     getData():any{
-      return this.http.get('https://run.mocky.io/v3/793607a0-cdbb-4af4-91be-41f694b94ba9').subscribe(res => console.log(res))
-        }
+    
+      
   ngOnInit(): void {
+ 
+    this.getData()
+  }
+  getData(){
+    return this.carService.getCars().subscribe((data:{})=>{console.log()})
   }
   
 
---
+++ car-shop/src/app/shared/cars.ts
@@ -0,0 +1,10 @@
+export interface Cars {
+id :number
+carImage:string
+year:string
+price:string
+milAge:string
+manufacturerName:string
+carType:string
+details:string
+}

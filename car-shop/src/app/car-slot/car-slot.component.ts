08dd8df1a9ab7e6e1fdc97e82da7e3f2fedc0886
mohammad AL-haj <mo7ammad_7aaj@hotmail.com>
2022-08-16 00:54:26 +0300

    xdfgxfgfgfg

--- car-shop/src/app/car-slot/car-slot.component.ts
+++ car-shop/src/app/car-slot/car-slot.component.ts
@@ -7,45 +7,44 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./car-slot.component.css']
 })
 export class CarSlotComponent implements OnInit {
-  cars =[]
-  
-  currentIndex:number=0
-  constructor(private http:HttpClient) {
+  cars = []
+
+  index: number = 0
+  constructor(private http: HttpClient) {
     this.getCar();
-    
+
   }
-    
+
   ngOnInit(): void {
   }
 
-getCar(){
-   this.http.get<any>("https://run.mocky.io/v3/d422663a-5f9e-4660-9707-43882be6fbbd").subscribe({
-    next: (resp) => {
-      this.cars = resp.cars;
-      console.log(this.cars)
+  getCar() {
+    this.http.get<any>("https://run.mocky.io/v3/d422663a-5f9e-4660-9707-43882be6fbbd").subscribe({
+      next: (resp) => {
+        this.cars = resp.cars;
 
-    },
-    error: () => {}
-   });
-  
-    
-}
- nextItem(){
-  if(this.currentIndex==this.cars.length-1){
-    this.currentIndex=0
-  }else{
-    this.currentIndex++;
-  } 
-  
- 
- }
- prevItem(){
-  if(this.currentIndex==0){
-    this.currentIndex=this.cars.length-1
-  }else{
-    this.currentIndex--;
+      },
+      error: () => { }
+    });
+
+
   }
- }
-  
+  nextItem() {
+    if (this.index == this.cars.length - 1) {
+      this.index = 0
+    } else {
+      this.index++;
+    }
+
+
+  }
+  prevItem() {
+    if (this.index == 0) {
+      this.index = this.cars.length - 1
+    } else {
+      this.index--;
+    }
+  }
+
 
 }
